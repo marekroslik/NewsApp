5aed69d081b86fe3f9f06082287b15236ad05f8b
Marek Roslik <marek.roslik@gmail.com>
2022-01-28 16:42:07 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Marek Roslik on 28.01.22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
